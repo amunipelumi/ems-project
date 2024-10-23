69c0e4754f8561b77a4fa5b92493b21243890a9c
amunipelumi <amunipelumi@gmail.com>
2024-10-23 15:01:29 +0100

    Adjusted

--- ems_api/core/config.py
+++ ems_api/core/config.py
@@ -11,6 +11,8 @@ HOST = str(os.getenv('REDIS_HOST'))
 PORT = int(os.getenv('REDIS_PORT'))
 DB = int(os.getenv('REDIS_DB'))
 
+MDB_USER = str(os.getenv('MONGO_INITDB_ROOT_USERNAME'))
+MDB_PASS = str(os.getenv('MONGO_INITDB_ROOT_PASSWORD'))
 MDB_HOST = str(os.getenv('MDB_HOST'))
 MDB_PORT = int(os.getenv('MDB_PORT'))
 
@@ -24,7 +26,12 @@ def redis_client():
         client.close()
 
 def mongo_client():
-    client = MongoClient(f'mongodb://{MDB_HOST}:{MDB_PORT}/')
+    if not TEST_MODE:
+        client = MongoClient(
+            f'mongodb://{MDB_USER}:{MDB_PASS}@{MDB_HOST}:{MDB_PORT}/'
+            )
+    else:
+        client = MongoClient(f'mongodb://{MDB_HOST}:{MDB_PORT}/')
     try:
         yield client
     finally:
